71bc1f54b28097383b0269c79327d614b116324c
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-11-28 13:55:55 +0200

    feat: install react-markdown package for post content text

--- components/Posts/PostDetail/PostDetail.js
+++ components/Posts/PostDetail/PostDetail.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Markdown from "react-markdown";
 import s from "./PostDetail.module.css";
 
 const DUMMY_POST = {
@@ -24,7 +25,7 @@ const PostDetail = () => {
           height={150}
         />
       </header>
-      {DUMMY_POST.content}
+      <Markdown>{DUMMY_POST.content}</Markdown>
     </article>
   );
 };
