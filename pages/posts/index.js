a1e5e741196578019a8eaaf64fca8495be25df95
ivankdev-fullstack <ivan.kononenko.dev@gmail.com>
2024-11-28 17:37:53 +0200

    feat: update posts utils

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,15 +1,47 @@
-import React from "react";
-import AllPosts from "../../components/Posts/AllPosts/AllPosts";
-import { getAllPosts } from "../../lib/posts";
+import fs from "fs";
+import path from "path";
 
-const PostsPage = ({ posts }) => {
-  return <AllPosts posts={posts} />;
-};
+import matter from "gray-matter";
 
-export const getStaticProps = () => {
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getPostsFiles() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getPostData(postIdentifier) {
+  const postSlug = postIdentifier.replace(/\.md$/, ""); // removes the file extension
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
+  const fileContent = fs.readFileSync(filePath, "utf-8");
+  const { data, content } = matter(fileContent);
+
+  const postData = {
+    slug: postSlug,
+    ...data,
+    content,
+  };
+
+  return postData;
+}
+
+export function getAllPosts() {
+  const postFiles = getPostsFiles();
+
+  const allPosts = postFiles.map((postFile) => {
+    return getPostData(postFile);
+  });
+
+  const sortedPosts = allPosts.sort((postA, postB) =>
+    postA.date > postB.date ? -1 : 1
+  );
+
+  return sortedPosts;
+}
+
+export function getFeaturedPosts() {
   const allPosts = getAllPosts();
 
-  return { props: { posts: allPosts } };
-};
+  const featuredPosts = allPosts.filter((post) => post.isFeatured);
 
-export default PostsPage;
+  return featuredPosts;
+}
